53b2844cf080e9f72fe2a6d07e41e32aa02d7c4d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-03 09:48:19 +0000

    P7 Final

--- p7final.c
+++ p7final.c
@@ -17,8 +17,45 @@ int find_gcd(int a,int b)
       a=t;
     }
   return a;
+  /*
+for(int i=1;i<=a && i<=b ;i++)
+{
+ if(a%i==0 && b%i==0)
+{
+ t=i;
+}
+}
+*/
+}
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Input fraction\n");
+  scanf("%d %d",&f.num,&f.den);
+  return f;
+}
+Fraction add_fraction(Fraction f1,Fraction f2)
+{
+  Fraction sum;
+  sum.num=f1.num*f2.den+f2.num*f1.den;
+  sum.den=f1.den*f2.den;
+  int g=find_gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
+void output(Fraction f1,Fraction f2,Fraction sum)
+{
+  printf("The sumof Two Fractions %d/%d and %d/%d is: %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
 }
 int main()
 {
-  printf("Gcd is %d \n",find_gcd(16,24));
+  int gcd;
+  Fraction f1,f2,result;
+  f1=input_fraction();
+  f2=input_fraction();
+  /*gcd=find_gcd();*/
+  result=add_fraction(f1,f2);
+  output(f1,f2,result);
+  return 0;
 }
