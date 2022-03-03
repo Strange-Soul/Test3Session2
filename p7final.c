9d859a7f0ac3d7f17fb70fa9b073656604a0b79b
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-03 08:55:32 +0000

    gcd

--- p7final.c
+++ p7final.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
 
+struct _fraction
+{
+ int num,den;
+};
+
+typedef struct _fraction Fraction;
+
+int find_gcd(int a,int b)
+{
+  int t;
+  while(b!=0)
+    {
+      t=b;
+      b=a%b;
+      a=t;
+    }
+  return a;
+}
+int main()
+{
+  printf("Gcd is %d \n",find_gcd(16,24));
+}
