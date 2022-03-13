e2d1fb2117c268bbb594fa8f3b22841cafa2f35d
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-13 16:35:02 +0000

    P4

--- p2final.c
+++ p2final.c
@@ -1,1 +1,47 @@
+#include<stdio.h>
 
+struct _fraction
+{
+ int num,den;
+};
+typedef struct _fraction Fraction;
+
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Enter a fraction to Find Largest \n");
+  scanf("%d %d",&f.num,&f.den);
+  return f;
+}
+Fraction Largest_fraction(Fraction f1,Fraction f2,Fraction f3)
+{
+  Fraction large;
+  int p=f1.num/f1.den;
+  int q=f2.num/f2.den;
+  int r=f3.num/f3.den;
+  if(p>q && p>r)
+  {
+    large = f1;
+  }
+  else if(q>r)
+  {
+    large=f2;
+  }
+  else
+    large=f3;
+  return large;
+}
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction Largest)
+{
+    printf("%d/%d is largest among %d/%d , %d/%d and %d/%d\n",Largest.num,Largest.den,f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
+  }
+int main()
+{
+  Fraction x,y,z;
+  x=input_fraction(); 
+  y=input_fraction();
+  z=input_fraction();
+  Fraction result=Largest_fraction(x,y,z);
+  output(x,y,z,result);
+  return 0;
+}
---
+++ p4final.c
@@ -0,0 +1,31 @@
+/* Write a program to evaluate a polynomial at a given point using horners method.*/
+int input_degree();
+float input_x();
+void input_coefficients(int n, float a[n]);
+float evaluate_polynomial(int n, float a[n], float x);
+void out_put(int n, float a[n], float x, float result);
+
+
+#include<stdio.h>
+int input_degree()
+{
+  
+}
+float input_x()
+{
+  
+}
+void input_coefficients(int n,float a[n])
+{
+  
+}
+float evaluate_polynomial(int n,float a[n],float x)
+{
+  
+}
+void out_put(int n, float a[n], float x, float result)
+{
+  
+}
+
+
