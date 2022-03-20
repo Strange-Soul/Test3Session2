e0c8059ada10cf8bad2e120c3f68bba29d010c73
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-20 14:03:44 +0000

    P2

--- p2final.c
+++ p2final.c
@@ -15,11 +15,11 @@ Fraction input_fraction()
 }
 Fraction Largest_fraction(Fraction f1,Fraction f2,Fraction f3)
 {
-  Fraction large;
+ /* Fraction large;
   int p=f1.num/f1.den;
   int q=f2.num/f2.den;
   int r=f3.num/f3.den;
-  if(p>q && p>r)
+  if(p<q && p<r)
   {
     large = f1;
   }
@@ -29,11 +29,22 @@ Fraction Largest_fraction(Fraction f1,Fraction f2,Fraction f3)
   }
   else
     large=f3;
-  return large;
+  return large;*/
+  float a,b,c;
+  a=f1.num/f1.den;
+  b=f2.num/f2.den;
+  c=f3.num/f3.den;
+  if(a<b && a<c)
+    return f1;
+  else if(b<c)
+    return f2;
+  else
+    return f3;
+  
 }
-void output(Fraction f1,Fraction f2,Fraction f3,Fraction Largest)
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction smallest)
 {
-    printf("%d/%d is largest among %d/%d , %d/%d and %d/%d \n",Largest.num,Largest.den,f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
+    printf("%d/%d is smallest among %d/%d , %d/%d and %d/%d \n",smallest.num,smallest.den,f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
   }
 int main()
 {
